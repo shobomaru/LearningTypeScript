3f2020ead1e452b0d2661c9f09f5d8f463a3949d
shobomaru <shobomaru@gmail.com>
2016-08-07 18:25:59 +0900

    Improve communication

--- tcp/server.pl
+++ tcp/server.pl
@@ -4,45 +4,77 @@ use strict;
 use warnings;
 use IO::Socket;
 
-# http://www.geekpage.jp/programming/perl-network/simple-tcp.php
-
 my $sock = new IO::Socket::INET(
 	Listen => 5,
 	LocalAddr => 'localhost',
 	LocalPort => 12345,
 	Proto => 'tcp',
-	Reuse => 1
+	ReuseAddr => 0
 ) or die;
+
+sub hoge($$) {
+	my ($conn, $token) = @_;
+
+	if ($token eq "spring") {
+		print $conn "Akebono\n";
+	}
+	elsif ($token eq "summer") {
+		print $conn "Yoru\n";
+	}
+	elsif ($token eq "fall") {
+		print $conn "Yuugure\n";
+	}
+	elsif ($token eq "winter") {
+		print $conn "Tsutomete\n";
+	}
+	elsif ($token eq "quit") {
+		print $conn "Quit\n";
+		return 1;
+	}
+	elsif ($token eq "\n") {
+		# Flush stream by client.
+	}
+	else {
+		print "Unknown\n";
+	}
+	0;
+}
 
 while (my $conn = $sock->accept()) {
 
 	print "Hello\n";
 	print $conn "Hello\n";
 
+	my $msg = "";
+
 	while (1) {
-		my $msg = <$conn> || "";
-		if ($msg eq "") {
+		my $m = <$conn> || "";
+		if ($m eq "") {
 			sleep 1;
+			next;
 		}
-		elsif ($msg eq 'spring') {
-			print $conn "Akebono\n";
+		print "Recv: $m\n";
+
+		$msg .= $m;
+		my @tokens = split /\0/, $msg;
+		my $isLastTokenValid = $msg =~ /\0$/;
+		if ($isLastTokenValid) {
+			$msg = "";
 		}
-		elsif ($msg eq 'summer') {
-			print $conn "Yoru\n";
+		else {
+			$msg = pop @tokens;
 		}
-		elsif ($msg eq 'fall') {
-			print $conn "Yuugure\n";
+
+		my $quit = 0;
+		foreach my $token (@tokens) {
+			$quit = hoge($conn, $token);
+			last if $quit;
 		}
-		elsif ($msg eq 'winter') {
-			print $conn "Tsutomete\n";
-		}
-		elsif ($msg eq 'quit') {
-			print $conn "Quit\n";
-			last;
-		}
+		last if $quit;
 	}
 
 	print "Close\n";
+	$conn->flush();
 	$conn->close();
 }
 
--- tcp/src/app.ts
+++ tcp/src/app.ts
@@ -4,17 +4,38 @@ import * as net from "net";
 
 var conn = net.connect(12345, "localhost");
 
+conn.on("connect", function() {
+	console.log("[Connected]");
+});
 conn.on("error", function(e) {
-	console.log("[Error]" + e + "\n");
+	console.log("[Error]" + e);
+	process.exit();
+});
+
+// Recv
+conn.on("data", function(d) {
+	console.log("[Receive]" + d);
+});
+// Close
+conn.on("end", function() {
+	console.log("[End]");
 	conn.destroy();
 	process.exit();
-})
+});
 
-conn.on("connect", function() {
-	console.log("[Connected]\n");
-})
+// Send
+conn.write("spring\0");
+conn.write("summer\0");
+conn.write("fall\0");
+conn.write("winter\0");
+conn.write("quit\0");
+// Flush
+setTimeout(function() {
+	conn.write("\n");
+}, 2 * 1000);
 
 setTimeout(function() {
+	console.log("Error: Quit command failed.");
 	conn.destroy();
 	process.exit();
-}, 4 * 1000);
+}, 8 * 1000);
