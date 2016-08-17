8489452755bc22d9f768223cd88c2e8c9f049143
shobomaru <shobomaru@gmail.com>
2016-08-17 22:57:31 +0900

    Handle send and recv.

--- tcp/src/app.ts
+++ tcp/src/app.ts
@@ -1,8 +1,10 @@
+"use strict";
+
 // Application code
 
 import * as net from "net";
 
-var conn = net.connect(12345, "localhost");
+var conn = net.connect(12345, "127.0.0.1");
 
 conn.on("connect", function() {
 	console.log("[Connected]");
@@ -13,7 +15,9 @@ conn.on("error", function(e) {
 });
 
 // Recv
-conn.on("data", function(d) {
+var recv: string;
+conn.on("data", function(d: Buffer) {
+	recv = d.toString("utf8");
 	console.log("[Receive]" + d);
 });
 // Close
@@ -23,19 +27,55 @@ conn.on("end", function() {
 	process.exit();
 });
 
+var promise = new Promise((resolve: () => void, reject: () => void) => {
+	resolve();
+});
+
 // Send
-conn.write("spring\0");
-conn.write("summer\0");
-conn.write("fall\0");
-conn.write("winter\0");
-conn.write("quit\0");
-// Flush
-setTimeout(function() {
+/*promise.then(() => {
+	conn.write("spring\0");
 	conn.write("\n");
-}, 2 * 1000);
+});*/
+
+var connId = setInterval(() => {
+	if (recv == null)
+		return;
+	var msgs = recv.split("\n");
+	if (msgs.length == 0)
+		throw "Error: Unknown token.";
+	var msg = msgs[0];
+	switch (msg)
+	{
+		case "Hello":
+			conn.write("spring\0\n");
+			break;
+		case "Akebono":
+			conn.write("summer\0\n");
+			break;
+		case "Yoru":
+			conn.write("fall\0\n");
+			break;
+		case "Yuugure":
+			conn.write("winter\0\n");
+			break;
+		case "Tsutomete":
+			conn.write("quit\0\n");
+			break;
+		case "Quit":
+			clearInterval(connId);
+			conn.destroy();
+			process.exit();
+			break;
+		default:
+			console.log("Unknown token.");
+			break;
+	}
+	recv = null;
+}, 500);
 
 setTimeout(function() {
 	console.log("Error: Quit command failed.");
+	clearInterval(connId);
 	conn.destroy();
 	process.exit();
-}, 8 * 1000);
+}, 60 * 1000);
